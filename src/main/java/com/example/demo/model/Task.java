38ac593373767b3d9d2cd8461de6d78c923252c1
Raunak Kumar Singh <kumarsinghraunak27@gmail.com>
2025-03-26 21:07:04 +0530

    debugged User and Task entity

--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -1,8 +1,10 @@
 package com.example.demo.model;
 
 import com.example.demo.constants.Status;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
 import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.time.LocalDate;
@@ -31,6 +33,7 @@ public class Task {
 
     @ManyToOne
     @JoinColumn(name = "user_id")
+    @JsonBackReference  // Prevents infinite recursion . "This annotation solved the bug of infinite recursion between users and tasks refer this chat of chatgpt https://chatgpt.com/share/67e3c7a5-aad8-800e-9365-cf33e408d877 "
     private User user;
 
 }
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -36,7 +37,8 @@ public class User {
     private String email;
     private String name;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference  // Prevents infinite recursion. "// Prevents infinite recursion . "This annotation solved the bug of infinite recursion between users and tasks refer this chat of chatgpt https://chatgpt.com/share/67e3c7a5-aad8-800e-9365-cf33e408d877 "
     private List<Task> tasks;
 
 }
